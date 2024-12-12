9634cadf3732e29c65908c996ccf884a9401daa8
Andrey1960-dot <andreyxiomi5plus@gmail.com>
2024-12-12 18:55:07 +0300

    Add files via upload

---
+++ module_9_7.py
@@ -0,0 +1,24 @@
+def is_prime(func):
+    def wrapper(*args, **kwargs):
+        total = func(*args, **kwargs)
+        n = 0
+        for i in range(1, total + 1):
+            if total % i == 0:
+                n += 1
+        if n != 0:
+            print('Простое')
+        else:
+            print('Составное')
+        return total
+    return wrapper
+
+@is_prime
+def sum_three(*args):
+    return sum(args)
+
+result = sum_three(2, 3, 6)
+print(result)
+
+
+
+
